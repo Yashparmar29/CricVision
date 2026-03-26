069fdf42c9ea3afd63a9fce882ea2113e386a2fc
yash parmar <parmaryas29@gmail.com>
2026-03-26 22:09:33 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+prefrom flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
